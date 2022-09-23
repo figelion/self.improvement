28edbcc4955f9081f966937864ecb740cc27b402
Adam Bugaj <Adam.Bugaj@mtupolska.com>
2022-09-23 12:14:55 +0200

    Add tests for withdraw functionality

--- python/bank_tdd/tests/test_account.py
+++ python/bank_tdd/tests/test_account.py
@@ -9,3 +9,19 @@ def test_deposition_made_for_account_adds_money_to_account() -> None:
     account.deposition(amount)
 
     assert account.balance == amount
+
+
+def test_withdraw_money_from_account_returns_money() -> None:
+    account = Account()
+    account.deposition(100)
+    withdraw_money = account.withdraw(50)
+
+    assert withdraw_money == 50
+
+
+def test_withdraw_money_from_account_updates_state_of_account() -> None:
+    account = Account()
+    account.deposition(100)
+    withdraw_money = account.withdraw(50)
+
+    assert account.balance == 50
